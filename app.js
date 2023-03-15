91383c958a21f0371dbc15684e7b82ca5fcaadd1
O_had <ohadkachlon@gmail.com>
2023-03-15 12:00:30 +0200

    add morgan for log node

--- app.js
+++ app.js
@@ -1,24 +1,41 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const morgan = require('morgan')
 
-app.use((req,res,next) =>{
-    res.header('Access-Control-Allow-Origin','*');
-    res.header('Access-Control-Allow-Headers','Origin,X-Requested-With,Content-Type,Accept,Authorization');
-    if(req.method ==='OPTIONS'){
-        res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
-        return res.status(200).json({});
-    }
-    next();
+app.use(morgan('dev'))
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT,POST,PATCH,DELETE,GET");
+    return res.status(200).json({});
+  }
+  next();
 });
 
-
-app.get('/',(req,res)=>{
-    res.status(200).json({
-    message:'Hello World'
-
-    })
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Hello World",
+  });
 });
 
+app.use((req, res, next) => {
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      massage: error.massage,
+    },
+  });
+});
 
 module.exports = app;
